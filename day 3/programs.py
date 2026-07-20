00ba6a278d5be09677ba55ade1f1e89e9cb36cd6
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-21 00:03:11 +0530

    Add day 3 practice files

---
+++ day 3/programs.py
@@ -0,0 +1,13 @@
+name = "abcde fghijk lmnopqrstuvwxyz"
+print(name[1:18:3])
+
+print(len(name)) 
+
+print(name.endswith("xyz"))
+
+print(name.upper())
+
+print(name.title())
+
+student = "fazil \nis a very \"good\" boy "
+print(student)

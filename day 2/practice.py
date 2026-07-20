c8c80a74f68506310f8ffa52aab9443be4212c90
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-20 20:43:40 +0530

    Initial commit

---
+++ day 2/practice.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20
+print(a+b)
+
+a = 22
+b = 5
+print("the remainder is:" , a % b)
+
+a = (input("enter the value of a : "))
+print(type(a))
+
+a = int(input("enter the value of a : "))
+b = int(input("enter the value of b : "))
+
+print("a is greater than b : " , a>b)
+
+a = int(input("enter the value of a : "))
+b = int(input("enter the value of b : "))
+
+print("the average of these two numbers is : " , (a+b)/2)
+
+a = int(input("enter the value of a : "))
+print( a**3)
---
+++ day 2/progarms.py
@@ -0,0 +1,14 @@
+a = 25
+print(type(a))
+
+b = "fazil"
+print(type(b))
+
+c = "2.4"
+print(float(a))
+
+A = int(input("enter number A : "))
+B = int(input("enter number B : "))
+# print("number A is :", A )
+# print("number B is :", B )
+print(" the sum is : " , A + B)

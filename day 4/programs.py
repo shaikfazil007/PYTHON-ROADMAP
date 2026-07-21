12ef272a3f78afe294c551ac7df15e6338258ba5
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-22 00:28:24 +0530

    Add day 4 Python practice

---
+++ day 4/programs.py
@@ -0,0 +1,29 @@
+# lists
+
+# friends = [ "fazil" , "manohar" , 26 , 3.40 , False, "irfan" ]
+# print(friends[0])
+
+# friends[1] = "asshu"    #lists are mutable
+# print(friends[1]) 
+# print(friends[1:4])
+
+# friends.append("faizaan")
+# print(friends)
+
+# list = [1, 2, 11, 8, 4, 9]
+# list.sort()
+# print(list) 
+# list.insert(2,24)
+# print(list)
+# print(list.pop(4))
+
+#tuples
+
+a = (1,)
+print(type(a))
+
+b = (1, "fazil", 2.40,False, "ashu" )
+print(b)
+
+no = b.count("fazil")
+print(no)

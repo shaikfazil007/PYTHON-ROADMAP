b25c84865b5b8dc90025242f82e2a1111f776087
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-22 21:27:53 +0530

    Update day 4 practice.py

--- day 4/practice.py
+++ day 4/practice.py
@@ -1,36 +1,36 @@
-#problem 1
+# problem 1
 
-# marks = []
+marks = []
 
-# f1 = int(input("enter marks here :" ))
-# marks.append(f1)
-# f2 = int(input("enter marks here :" ))
-# marks.append(f2)
-# f3= int(input("enter marks here :" ))
-# marks.append(f3)
-# f4 = int(input("enter marks here :" ))
-# marks.append(f4)
-# f5 = int(input("enter marks here :" ))
-# marks.append(f5)
-# f6 = int(input("enter marks here :" ))
-# marks.append(f6)
-# f7 = int(input("enter marks here :" ))
-# marks.append(f7)
-# marks.sort()
-# print(marks)
+f1 = int(input("enter marks here :" ))
+marks.append(f1)
+f2 = int(input("enter marks here :" ))
+marks.append(f2)
+f3= int(input("enter marks here :" ))
+marks.append(f3)
+f4 = int(input("enter marks here :" ))
+marks.append(f4)
+f5 = int(input("enter marks here :" ))
+marks.append(f5)
+f6 = int(input("enter marks here :" ))
+marks.append(f6)
+f7 = int(input("enter marks here :" ))
+marks.append(f7)
+marks.sort()
+print(marks)
 
-#problem 3
+# problem 3
 
-# a= (1, "fazil", 2.44, "harry")
-# a[2]="larry"
-# print(a)
+a= (1, "fazil", 2.44, "harry")
+a[2]="larry"
+print(a)
 
-#problem 4
+# problem 4
 
-# l = [2, 4, 12, 11]
-# print(sum(l))
+l = [2, 4, 12, 11]
+print(sum(l))
 
-#problem 5
+# problem 5
 
 a = (7, 0, 8, 0, 0, 9)
 c = a.count(0)

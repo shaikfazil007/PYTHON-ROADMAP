ed9adc9a85cafbf05939bb89f488ec03283bc6b0
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-24 20:50:41 +0530

    Add day 6 conditionals

---
+++ day 6/conditionals.py
@@ -0,0 +1,11 @@
+#CONDITIONALS STATEMENTS
+
+a =int(input("enter your age : "))
+if(a>=18):
+    print("you can drive the vehicle")
+elif(a<0):
+    print("invalid age")
+else:
+    print("you can't drive the vehicle")
+    
+print("This is the 'RTO' rule ")    

e888c2d6072c653507ddbdd45094800164ad8b47
Shaik Fazil <shaikfaroq413@gmail.com>
2026-07-24 20:55:40 +0530

    Add day 6 multiple if statements

---
+++ day 6/multiple_if's.py
@@ -0,0 +1,24 @@
+a =int(input("enter your age : "))
+
+#IF STATEMENT NO : 1
+
+if(a%2 ==0):
+    print("its an even number")
+else:
+    print("its an odd number")
+    
+#IF STATEMENT NO : 2
+    
+if(a>=18):
+    print("you can drive the vehicle")
+    
+elif(a<0):
+    print("invalid negative age")
+    
+elif(a==0):
+    print("you are entering an invalid age")
+    
+else:
+    print("you can't drive the vehicle")
+    
+print("This is the 'RTO' rule ")   

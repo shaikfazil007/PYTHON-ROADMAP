53bc3318837263fdf8261fd678141519b065e4a3
Shaik Fazil <shaikfaroq413@gmail.com>
2026-08-07 22:32:28 +0530

    Add day 7 for loop

---
+++ day 7/for_loop.py
@@ -0,0 +1,21 @@
+for i in range(0,101,10):
+    print(i)
+    
+l= [1, 2, 34, 78, 1253,112]
+for i in l:
+    print(i)
+    
+t = ( 1 , 252, 183, 1882)
+for i in t:
+    print(i)
+
+s = "fazil"
+for i in s:
+    print(i)
+    
+    
+l = [1, 45, 44]
+for item in l:
+    print(item)
+else:
+    print("done")

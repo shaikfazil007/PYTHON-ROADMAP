ccadb68139ec24bb740f03e1d6ecb0f1da0c2298
Shaik Fazil <shaikfaroq413@gmail.com>
2026-08-20 21:03:40 +0530

    Add day 8 recursion

---
+++ day 8/03_recursion.py
@@ -0,0 +1,12 @@
+
+
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n*factorial(n-1)
+
+n = int(input("enter a number:"))
+print(f"factorial of the given number is : {factorial(n)}")
+ 
+
+

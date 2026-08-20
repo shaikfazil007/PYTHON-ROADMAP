cd736bd1d0209bd7090dcbb7453d96e874405eba
Shaik Fazil <shaikfaroq413@gmail.com>
2026-08-20 21:02:06 +0530

    Add day 8 functions

---
+++ day 8/01_functions.py
@@ -0,0 +1,28 @@
+def funavg():
+    a=int(input("enter marks : "))
+    b=int(input("enter marks : "))
+    c=int(input("enter marks : "))
+    average = (a+b+c)/3
+    print(average)
+funavg()
+funavg()
+funavg()
+funavg()
+funavg()    
+    
+    
+def GoodDay():
+    print("GoodDay")
+GoodDay()
+
+
+def GoodDay(name, ending):
+    print("GoodDay"+name)
+    print(ending)
+    return("ok")
+
+a = GoodDay(" fazil","thank you !")
+print(a)
+
+GoodDay(" chintu","thank you !")
+GoodDay(" asshu","thanks !")

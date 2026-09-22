7d38aeccfe3b2d00b1fdbda351aacc01acbf92c1
Shaik Fazil <shaikfaroq413@gmail.com>
2026-09-22 22:36:32 +0530

    Add day 11 class

--- day 11/01_class.py
+++ day 11/01_class.py
@@ -3,4 +3,7 @@ class employee:
     language = "python"
     salary = 10000
 fazil  = employee()
-print(fazil.name,fazil.salary)    
+print(fazil.name,fazil.salary)   
+
+rohan = employee()
+print(rohan.salary,rohan.language)
